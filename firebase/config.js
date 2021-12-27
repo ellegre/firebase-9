e1ee07769b3a59f3efb41e1b60d74dbfda580cd6
Liubou Grebennikova <liubou.grebennikova@gmail.com>
2021-12-27 10:47:27 +0200

    set up firestore

---
+++ firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD77w9g6p5PU89o3FwOpp2SX4ofFgNcZV0",
+  authDomain: "reading-list-app-d9bfe.firebaseapp.com",
+  projectId: "reading-list-app-d9bfe",
+  storageBucket: "reading-list-app-d9bfe.appspot.com",
+  messagingSenderId: "772118535153",
+  appId: "1:772118535153:web:62980c346c77982d33c250"
+};
+
+// init firebase
+initializeApp(firebaseConfig)
+
+// init firestore
+const db = getFirestore()
+
+export { db }
